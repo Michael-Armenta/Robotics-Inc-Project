d3ef09bd41fc474acae38462b2a48522793f2ba0
Michael-Armenta <m.armenta271@gmail.com>
2023-12-15 16:32:12 -0700

    updated style

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -4,9 +4,9 @@ export default function Success(props) {
       <div className="relative py-16 bg-blueGray-200 h-100">
         <div className="text-center pt-28">
           <section className="relative py-16 bg-blueGray-200">
-          <h1 className="text-xl mb-5 w-fit mx-auto px-4 py-1 text-black font-large">
-                Transaction Successful
-              </h1>
+            <h1 className="text-xl mb-5 w-fit mx-auto px-4 py-1 text-black font-large">
+              Transaction Successful
+            </h1>
             <div className="container mx-auto px-4">
               <p className="px-4 text-center">Thank you for your purchase</p>
             </div>
